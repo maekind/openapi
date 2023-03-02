3d75b5be6efe6ca9069dc36d526858c00ed7fce3
marco <marco@marcoespinosa.es>
2023-03-02 21:49:20 +0100

    Add possible endpoints

--- vimeo/apiv1/urls.py
+++ vimeo/apiv1/urls.py
@@ -6,4 +6,13 @@ from . import views
 urlpatterns = [
     path('<int:pk>/', views.video_detail_apiview),
     path('', include('video.urls')),
+    # path('<int:pk>/best_quality/', views.xxx), => Return {id: {quality: url}}
+    # path('<int:pk>/lowest_quality/', views.xxx), => Return {id: {quality: url}}
+    # path('<int:pk>/qualities/', views.xxx), => Return {id: {quality1: url1, ..., qualityn: urln}}
+    # path('<int:pk>/basic_info/', views.xxx), {id: {some basic propperties}}
+    # path('<int:pk>/owner/', views.xxx), {id: {owner info}}
+    # path('<int:pk>/bestquality/', views.xxx),
+
+    # I can only retrieve information related to one video, because I don't clone Vimeo database.
+    # My database grows with each api request.
 ]
